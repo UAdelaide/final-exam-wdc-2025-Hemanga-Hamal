3975c48229decafe0652d602b81755129d64f9ff
Hemanga-Hamal <a1923168@adelaide.edu>
2025-06-20 14:03:13 +0000

    [automatic commit] 2025-06-20 14:03:13 UTC 61.245.156.225

--- part2/app.js
+++ part2/app.js
@@ -26,4 +26,19 @@ app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
 
+    // GET /api/dogs
+app.get('/dogs', async (req, res) => {
+      try {
+        const [rows] = await db.query(`
+          SELECT Dogs.name AS dog_name, Dogs.size, Users.username AS owner_username
+          FROM Dogs
+          JOIN Users ON Dogs.owner_id = Users.user_id
+        `);
+        res.json(rows);
+      } catch (err) {
+        res.status(500).json({ error: 'Failed to fetch dogs' });
+      }
+    });
+
+
 module.exports = app;
