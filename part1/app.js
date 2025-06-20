9855ea7c999908da6019746bd9a962c14c6baef7
Hemanga-Hamal <a1923168@adelaide.edu>
2025-06-20 02:44:08 +0000

    [automatic commit] 2025-06-20 02:44:08 UTC 61.245.156.225

--- part1/app.js
+++ part1/app.js
@@ -34,7 +34,14 @@ let db;
 
     //sample data because i am smart and this prac sucks
     const [[{count;userCount}]] = await db.query('SELECT COUNT(*) AS userCount FROM Users');
-    
+    if (userCount === 0) {'
+        'INSERT INTO Users (username, email, password_hash, role) VALUES
+        ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+        ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+        ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+        ('newwalker', 'new@example.com', 'hashed999', 'walker'),
+        ('emilyowner', 'emily@example.com', 'hashed888', 'owner')
+      `
 
 })();
 
