6ae41b475a749f53276c2cdbd105d6e3eb3ca0ef
Hemanga-Hamal <a1923168@adelaide.edu>
2025-06-20 14:58:42 +0000

    [automatic commit] 2025-06-20 14:58:42 UTC 61.245.156.225

--- part2/app.js
+++ part2/app.js
@@ -34,7 +34,7 @@ app.use('/api/users', userRoutes);
 app.get('/api/dogs', async (req, res) => {
       try {
         const [rows] = await db.query(`
-          SELECT *
+          SELECT Dogs.name AS dog_name, Dogs.size, Users.username AS owner_username
           FROM Dogs
           JOIN Users ON Dogs.owner_id = Users.user_id
         `);
