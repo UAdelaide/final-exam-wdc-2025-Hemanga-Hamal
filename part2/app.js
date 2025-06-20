fec3ceea74894f408320d9a2c9953bf842b39b75
Hemanga-Hamal <a1923168@adelaide.edu>
2025-06-20 14:08:07 +0000

    [automatic commit] 2025-06-20 14:08:07 UTC 61.245.156.225

--- part2/app.js
+++ part2/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
-const db = require('../models/db');
+const db = require('./db');
+
 require('dotenv').config();
 
 const app = express();
