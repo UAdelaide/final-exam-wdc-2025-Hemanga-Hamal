e4af53d67a5599723c3d3e6e3779cba22bd39139
Hemanga-Hamal <a1923168@adelaide.edu>
2025-06-20 14:02:18 +0000

    [automatic commit] 2025-06-20 14:02:18 UTC 61.245.156.225

--- part2/app.js
+++ part2/app.js
@@ -21,6 +21,7 @@ app.use(session({
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
