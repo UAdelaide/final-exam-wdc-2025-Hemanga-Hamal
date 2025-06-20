c0e085ad1530657f63230f961ecf2fb48b429248
Hemanga-Hamal <a1923168@adelaide.edu>
2025-06-20 02:54:32 +0000

    [automatic commit] 2025-06-20 02:54:32 UTC 61.245.156.225

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -2,4 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/dogs', async (req, res) => {
-    
+    try {
+        const db = req.app.locals.db;
+        const [dogs] = await db.execute('SELECT * FROM Dogs');
+        res.json(dogs);
+    } catch (err) {
+        console.error('Error fetching dogs:', err);
